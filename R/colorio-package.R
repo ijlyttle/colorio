f12f5e41d3ef2dd75711744010d786bcf6ad193f
Ian Lyttle <ian.lyttle@schneider-electric.com>
2020-10-20 19:57:00 -0500

    Add package documentation

---
+++ R/colorio-package.R
@@ -0,0 +1,13 @@
+#' colorio: package to wrap colorio Python package
+#'
+#' This package offers low-level access to the
+#' [colorio](https://github.com/nschloe/colorio) Python package, using the
+#' [reticulate](https://rstudio.github.io/reticulate) package.
+#'
+#' The immediate motivation is to provide R users wuth access to the wide
+#' array of color spaces available in colorio, including many newer ones like
+#' CIECAM02, CAM16, and Jzazbz.
+#'
+#' @name colorio-package
+#'
+NULL
